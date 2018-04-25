0388d872d09f83e96bdceb06118bad811ea49fff
Diego Negrelli <diegon@gmail.com>
2018-04-25 17:21:16 -0300

    initial commit

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,10 +1,22 @@
 package hello;
 
+import java.net.InetAddress;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+	
+    @RequestMapping("/")
+    public String home() throws Exception {
+    	System.out.println("Servindo um hello bem de boas...");
+        return "Hello iBeer! - proudly served by " + InetAddress.getLocalHost().getHostName();
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
